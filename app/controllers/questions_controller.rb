48b671bdedab73f485215aded7b48e7fbee3f44c
natsuki-tacica <sns.senyou.n@gmail.com>
2020-01-20 02:07:59 +0900

    add コード修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,13 @@
 class QuestionsController < ApplicationController
-  # before_action :find_question, only: :show
-
-  # def show
-  #   c = @survey.users.eager_load(:ansers).find_by uuid: session[:uuid]
-  #   @anser = c.ansers.where(question_id: @question.id).first_or_initialize
-  # end
-
-  # private
-  # def find_question
-  #   @question = Question.joins(:surveys).includes(:surveys).find params[:id]
-  #   @survey = @question.surveys
-  # end
-
   before_action :set_group
 
   def index
-    @group = Group.find(params[:group_id])
-    # urlにgroup_id(params)がある場合
-    if params[:group_id]
-      # Groupのデータベースのテーブルから一致するidを取得
-      @group = Group.find(params[:group_id])
-      # group_idと紐づく投稿を取得
+    @group = Group.find(params[:group_id]) # urlにgroup_id(params)がある場合
+    if params[:group_id] # Groupのデータベースのテーブルから一致するidを取得
+      @group = Group.find(params[:group_id]) # group_idと紐づく投稿を取得
       @questions = @group.questions.page(params[:page]).per(20)
     else
-      # 投稿すべてを取得
-      @questions = Question.all
+      @questions = Question.all # 投稿すべてを取得
     end
     
     @anser = Anser.new
