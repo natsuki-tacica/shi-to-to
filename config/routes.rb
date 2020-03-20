3d667233397dc759d2ec8e600cb96bea5ada6872
natsuki-tacica <sns.senyou.n@gmail.com>
2020-03-20 14:09:27 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users  
-  root to: 'groups#index'
+  get to: 'groups#index'
   resources :users, only: [:index, :edit, :update, :show] #ユーザー周りきれいにする
   resources :groups,only:[:index] do
     resources :questions,only:[:index] do
